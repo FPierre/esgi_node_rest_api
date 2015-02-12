7ba8357a29aaf20c315b77de3d448a6d50d989eb
FPierre <pflauder@gmail.com>
2015-02-12 12:25:43 +0100

    Ajout de l'appel du Controller de gestion des emprunts

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = function(server) {
+    require('./borrow/borrow')(server);
     require('./elements/elements')(server);
     require('./logIn')(server);
     require('./logOut')(server);
